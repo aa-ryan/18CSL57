c5cfe35644d9c40cf4aa76e7afee636653be79c4
aa-ryan <aryansinha6363@gmail.com>
2022-01-31 19:49:38 +0530

    Leaky Bucket added

---
+++ PART-B/PG6/Leaky.java
@@ -0,0 +1,67 @@
+import java.util.*; 
+public class Leaky 
+{ 
+	static int min(int x, int y) 
+	{ 
+		if(x < y) 
+			return x; 
+		else 
+			return y; 
+	} 
+
+	public static void main(String[] args) 
+	{
+		int drop=0, psent, nsec, cap, pleft=0, i, process;
+		int inp[] = new int[25];
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter The Bucket Size: ");
+		cap = sc.nextInt();
+		System.out.print("Enter The Operation Rate: ");
+		process = sc.nextInt();
+		System.out.print("Enter The No. Of Seconds You Want To Simulate: ");
+		nsec = sc.nextInt();
+		for(i = 0; i < nsec; i++)
+		{ 
+			System.out.print("Enter The Size Of The Packet Entering At "+ (i+1) + " sec: ");
+			inp[i] = sc.nextInt();
+		}
+		System.out.println("\nSecond | Packet Recieved | Packet Sent | Packet Left | Packet Dropped |\n");
+		System.out.println("------------------------------------------------------------------------\n");
+		for(i = 0; i < nsec; i++)
+		{ 
+			pleft += inp[i];
+			if(pleft > cap)
+			{ 
+				drop = pleft-cap;
+				pleft = cap;
+			}
+			System.out.print(i+1);
+			System.out.print("\t\t" + inp[i]);
+			psent = min(pleft,process);
+			System.out.print("\t\t" + psent);
+			pleft = pleft - psent;
+			System.out.print("\t\t" + pleft);
+			System.out.print("\t\t" + drop);
+			drop = 0;
+			System.out.println();
+		}
+
+		for(;pleft != 0; i++)
+		{
+			if(pleft > cap)
+			{
+				drop = pleft - cap;
+				pleft = cap;
+			}
+			System.out.print(i + 1);
+			System.out.print("\t\t0");
+			psent = min(pleft,process);
+			System.out.print("\t\t" + psent);
+			pleft = pleft - psent;
+			System.out.print("\t\t" + pleft);
+			System.out.print("\t\t" + drop);
+			System.out.println();
+		}
+	}
+
+}
