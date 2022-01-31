7cb12720480d1855d6b8ea6e1da0076a41d91c65
aa-ryan <aryansinha6363@gmail.com>
2022-01-31 19:09:19 +0530

    Client pg4

---
+++ PART-B/PG4/Client.java
@@ -0,0 +1,35 @@
+import java.net.*;
+
+public class Client
+{
+	public static void main(String[] args)
+	{
+
+		DatagramSocket skt;
+		try
+
+		{
+
+			skt = new DatagramSocket(); 
+			String msg = "Text Message "; 
+			byte[] b = msg.getBytes();
+
+			InetAddress host = InetAddress.getByName("127.0.0.1"); 
+			int  serverSocket=6788;
+
+			DatagramPacket request = new DatagramPacket(b,b.length,host,serverSocket); 
+			skt.send(request);
+			byte[] buffer = new byte[1000];
+
+			DatagramPacket reply = new DatagramPacket(buffer,buffer.length); 
+			skt.receive(reply);
+
+			System.out.println("Client received:" + new String(reply.getData())); 
+			skt.close();
+
+		}
+
+		catch(Exception ex) {}
+
+	}
+}
